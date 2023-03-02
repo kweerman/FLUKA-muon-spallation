8dacb0087f688e4ce5ee1982fd7d6089d1429af2
kweerman <kweerman@nikhef.nl>
2023-03-02 13:55:45 +0100

    mgdraw.f changed to use usdraw part for isotopes

---
+++ python_scripts/usdrawisotopes_reader.py
@@ -0,0 +1,116 @@
+# Version 1 2023 Kelly Weerman 
+# from the "outfile_isotopes" file created with vers5_eventscreator.py the information is substracted
+# which is totAZ_count ([A,Z,count]), AZ_jtrack([A,Z],[jtrack],[count]), part_gen_count ([[A,Z],[particles_lists],[counts]])
+# and [particle_lists] = [ [jtrack, 6,10,[4,2]] , [...], ] where the last number inbetween brackets, i.e. [4,2], is the A Z of daugthers
+# return totAZ_count, AZ_jtrack, interactions_list, no_muons in the form [A, Z, count], [[A,Z], [jtracks], [counts]]
+# [[A,Z], [particles lists], [counts]] where particle lists [jtrack, 6,10,[4,2]] , [...], ... and no_muons
+# the created lists (in unformatted "isotope_filename") can be printed with "usdraw_createdaughterfile.ipynb"
+
+import pickle
+from operator import itemgetter
+import csv
+import numpy as np
+import sys
+
+
+def pickleLoader(pklFile):
+    try:
+        while True:
+            yield pickle.load(pklFile)
+    except EOFError:
+        pass
+
+
+def extract_first(lijst):
+    # returns the first value of each sublist in a list of lists
+    return list(map(itemgetter(0), lijst))
+
+
+def addinfo_tolists(variable, count, list_ind, return_list):
+    # return list of the form [[..], [variables], [counts]]
+    # checks if the variable is in return_list[ind][1] and adds corresponding count
+
+    if variable not in return_list[list_ind][1]:
+        return_list[list_ind][1].append(variable)
+        return_list[list_ind][2].append(count)
+    else:
+        variable_ind = return_list[list_ind][1].index(variable)
+        return_list[list_ind][2][variable_ind] += count
+    
+    return return_list
+
+
+def isotope_list_count(path, file_name, isotope_filename, job_range=[1,2]):
+
+    #AZ_jtrack = []
+    interactions_list = []
+    no_muons = 0
+    totAZ, totAZ_count = [], []
+    AZ_jtrack = []
+            
+    for job in range(job_range[0], job_range[1] + 1):
+        try:
+            file = open(path + file_name + "%i"%(job),'rb')
+        except FileNotFoundError:
+            continue
+
+        no_muons += 1
+        print("Opened ", file_name + "%i"%(job))
+
+        # return all the information from the files, and add them
+        for event in pickleLoader(file):
+            isotope_list, part_gens, part_gen_count = event
+        file.close()
+
+        # the amount of isotopes created is the length of AZ list
+        for i in range(len(isotope_list)):
+            A, Z, count = isotope_list[i]
+            
+            # create a list with the count per isotope
+            isotope = [A, Z]
+            interactions = part_gens[i][1:]
+            jtracks = extract_first(interactions)
+            count_interactions = part_gen_count[i][1:]
+
+            if isotope not in totAZ:
+                totAZ.append(isotope)
+                totAZ_count.append([A, Z, count])
+
+                # in the form[[A,Z],[particles_lists],[counts]]
+                interactions_list.append([isotope, interactions, count_interactions])
+                
+                # create the jtrack list of the form [[A,Z],[jtracks],[counts]]
+                AZ_jtrack.append([isotope, [], []])
+                for i in range(len(interactions)):
+                    # index is -1 since we just added the AZ to the list
+                    list_ind, jtrack, jtrack_count = -1, jtracks[i], count_interactions[i]
+                    AZ_jtrack = addinfo_tolists(jtrack, jtrack_count, list_ind, AZ_jtrack)
+                
+            else:
+                ind = totAZ.index(isotope)
+                totAZ_count[ind][-1] += count 
+
+                # the particle creation and parent lists are updated
+                for j in range(len(interactions)):
+                    interaction, count_interaction = interactions[j], count_interactions[j]
+                    interactions_list = addinfo_tolists(interaction, count_interaction, ind, interactions_list)
+
+                    jtrack, jtrack_count = jtracks[j], count_interactions[j]
+                    AZ_jtrack = addinfo_tolists(jtrack, jtrack_count, ind, AZ_jtrack)
+    
+    file.close()
+
+    # return the total lists in a file for easier plotting
+    isotope_file = open(isotope_filename,'wb')
+    pickle.dump([totAZ_count, AZ_jtrack, interactions_list, no_muons], isotope_file)
+    print("The isotope list for {0} muons is given by {1}".format(no_muons, totAZ_count))
+    return totAZ_count, AZ_jtrack, interactions_list, no_muons
+
+
+path = "/dcache/xenon/kweerman/NewFlukaVersion/"
+file_name = "out_muonsXeLSLong_isotopes"
+job_range = [1,150]
+# the isotope file is a file that contains all the added counts from 150 tuns
+isotope_filename = sys.argv[1]
+isotope_list_count(path, file_name, isotope_filename, job_range)
+
---
+++ python_scripts/vers5_eventscreator.py
@@ -0,0 +1,119 @@
+# Version 5 2023 Kelly Weerman
+# reads SRCEFILE created with usdraw version 1 and saves the daughter particles
+# to a list of the form part_gens = [[A,Z], particles lists] where particle lists [jtrack, 6,10,[4,2]] , [...], ...
+# also returns count list corresponding to the particle lists in the form part_gen_count = [[A,Z], count] 
+# "isotope_filename" is filled with one line: totAZ_count, part_gens, part_gen_count
+
+import numpy as np
+import sys
+import pickle
+
+
+def events_creator(filename, isotope_filename):
+    # reads the SRCEFILE and creates lists of parent and daughter particles 
+    # together with the count, for a certain area of space
+
+    dataTypeStart = np.dtype([
+        ('Icode', np.int32),
+        ('Partcode', np.int32),
+        ('Jtrack', np.int32),
+    ])
+
+    dataTypeCoord = np.dtype([
+        ('Xtrack', np.float64),
+        ('Ytrack', np.float64),
+        ('Ztrack', np.float64),
+    ])
+
+    dataTypeAZ = np.dtype([
+        ('A', np.int32),
+        ('Z', np.int32),
+        ('Np', np.int32),
+    ])
+
+    particle_list = []
+    totAZ, totAZ_count = [], []
+    part_gens, part_gen_count = [], []
+
+    try:
+        file = open(filename,'rb')
+    except IOError:
+        raise Exception('Fail to open file')
+
+    # the first line has different bytes than the rest
+    lines = file.read(4)
+    header = np.fromfile(file,dtype='a28',count=1)[0]
+    print(header.decode())
+    no_events = 0
+
+    while lines:
+        lines = file.read(8)
+
+        # the last line of the file is empty
+        data = np.fromfile(file,dtype=dataTypeStart,count=1)
+        if len(data) == 0:
+            break
+        
+        icode, partcode, jtrack = data[0]
+        particle_list = [jtrack]
+        new_isotope = True
+
+        # a new event line is indicated with the following
+        if icode == 0.:
+            no_events += 1
+            continue
+
+        x0, y0, z0 = np.fromfile(file,dtype=dataTypeCoord,count=1)[0]
+        A, Z, n_part = np.fromfile(file,dtype=dataTypeAZ,count=1)[0]
+
+        # the number of KPART particles is indicated with np
+        if n_part > 0:
+            kpart_parts = np.fromfile(file,dtype=np.int32,count=n_part)
+            particle_list.extend(kpart_parts.tolist())
+
+        # then the heavy particles
+        # in this case we return the A, Z so twice as many counts
+        npheav = np.fromfile(file,dtype=np.int32,count=1)[0]
+        if npheav > 0:
+            kheavy_part = np.fromfile(file,dtype=np.int32,count=2*npheav)
+            particle_list.append(kheavy_part.tolist())
+
+        # we don't count the de-excitation as new isotopes and
+        # not the whole cylinder is tested
+        if icode != 106 and (z0 > 1000 and z0 < 3500):
+            isotope = [A, Z]
+            in_list = False
+            if isotope not in totAZ:
+                totAZ.append(isotope)
+                totAZ_count.append([A, Z, 1])
+
+                part_gens.append([isotope, particle_list])
+                part_gen_count.append([isotope, 1])
+
+            else:
+                ind = totAZ.index(isotope)
+                totAZ_count[ind][-1] += 1
+
+                no_reactions = 0
+                for part_range in part_gens[ind][1:]:
+                    no_reactions += 1
+                    if particle_list == part_range:
+                        part_gen_count[ind][no_reactions] += 1
+                        in_list = True
+                        break
+                if in_list == False:
+                    part_gens[ind].append(particle_list)
+                    part_gen_count[ind].append(1)
+
+        
+    file.close()
+    isotope_file = open(isotope_filename,'wb')
+    pickle.dump([totAZ_count, part_gens, part_gen_count], isotope_file)
+    isotope_file.close()
+    print("The isotope list is given by {0}".format(totAZ_count))
+    return
+
+file_name, isotope_filename = sys.argv[1], sys.argv[2]
+#file_name = "muons_XeLSLong001_SRCEFILE"
+#isotope_filename = "important"
+events_creator(file_name, isotope_filename)
